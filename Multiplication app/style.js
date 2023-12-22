10f0c619c10efa5765005dca040d8de733447f15
Bhagyashri V Zade <125757446+Code-Eagl@users.noreply.github.com>
2023-12-22 15:25:35 +0530

    Add files via upload
    two variable multiplication app mini project

---
+++ Multiplication app/style.js
@@ -0,0 +1,28 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+let score = JSON.parse(localStorage.getItem("score"));
+if (!score) {
+  score = 0;
+}
+scoreEl.innerText = "score: " + score;
+
+questionEl.innerText = "What is " + num1 + " multiply by " + num2 + " ?";
+
+const correctAns = num1 * num2;
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value();
+  if (userAns === correctAns) {
+    score++;
+    updatelocalstg();
+  } else {
+    score--;
+    updatelocalstg();
+  }
+});
+function updatelocalstg() {
+  localStorage.setItem("score", JSON.stringify(score));
+}
